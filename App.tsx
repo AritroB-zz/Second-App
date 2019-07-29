4e938b7c915788545f6072a80ac941f0bc53a979
Aritro Basu <abasuusa2010@gmail.com>
2019-07-29 15:51:26 -0500

    second commit

---
+++ App.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.myText}>Hello Ma! #crushingit </Text>
+    </View>
+
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'lightgreen',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  myText: {
+    backgroundColor: 'red',
+  }
+});
+
+
+
